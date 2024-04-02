a71ed43a4187cd233d75b984356a8de400b65cb9
G-venkatesh-1 <venkatesh.guduru@multicorewareinc.com>
2024-04-02 15:59:54 +0530

    add files

--- src/process.py
+++ src/process.py
@@ -136,8 +136,9 @@ def evaluate(model, test_loader, epoch, writer, encoder, nms_threshold, mtype,rt
                     plabel = torch.tensor(output2_np).cuda()
             elif rt == "val":
                 ploc,loc,label,prob = model(img)
+                print(ploc.shape,loc.shape,label.shape,prob.shape)
                 # ploc, plabel = ploc.float(), plabel.float()
-            for idx in range(len(ploc)):
+            # for idx in range(len(ploc)):
                 # ploc_i = ploc[idx, :, :].unsqueeze(0)
                 # plabel_i = plabel[idx, :, :].unsqueeze(0)
                 # # try:
@@ -148,20 +149,20 @@ def evaluate(model, test_loader, epoch, writer, encoder, nms_threshold, mtype,rt
                 # if img_size is None: #change
                 #     print("no image")
                 #     continue     
-                height, width = img_size[idx]                
+                # height, width = img_size[idx]                
                 # loc, label, prob = [r.cpu().numpy() for r in result]
-                for loc_, label_, prob_ in zip(loc, label, prob):
-                    detections.append([img_id[idx], loc_[0] * width, loc_[1] * height, (loc_[2] - loc_[0]) * width,
-                                    (loc_[3] - loc_[1]) * height, prob_,
-                                    category_ids[label_ - 1]])
+                # for loc_, label_, prob_ in zip(loc, label, prob):
+                #     detections.append([img_id[idx], loc_[0] * width, loc_[1] * height, (loc_[2] - loc_[0]) * width,
+                #                     (loc_[3] - loc_[1]) * height, prob_,
+                #                     category_ids[label_ - 1]])
 
-    detections = np.array(detections, dtype=np.float32)
+    # detections = np.array(detections, dtype=np.float32)
 
-    coco_eval = COCOeval(test_loader.dataset.coco, test_loader.dataset.coco.loadRes(detections), iouType="bbox")
-    coco_eval.evaluate()
-    coco_eval.accumulate()
-    coco_eval.summarize()
+    # coco_eval = COCOeval(test_loader.dataset.coco, test_loader.dataset.coco.loadRes(detections), iouType="bbox")
+    # coco_eval.evaluate()
+    # coco_eval.accumulate()
+    # coco_eval.summarize()
 
-    writer.add_scalar("Test/mAP", coco_eval.stats[0], epoch)
+    # writer.add_scalar("Test/mAP", coco_eval.stats[0], epoch)
 
 
